1e19941763ffb6d1a29e2e8487fccc94688796e6
saramartinn <145117681+saramartinn@users.noreply.github.com>
2023-09-27 10:36:37 +0200

    Add files via upload

---
+++ mp3.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void main() 
+{
+  
+  pid_t p2, p3;
+  
+  p2 = fork();
+  
+  if (p2 == 0 ) 
+  {
+    printf("Me voy a dormir\n ");
+    sleep(10);
+    printf("Despierto \n ");
+  }
+  else{
+  wait(NULL);
+  p3 = fork();
+  
+	  if (p3 == 0)
+	   {
+	      wait(NULL);
+	      printf(" Soy el P3 y mi PID es = %d, y el de mi padre es = %d\n", getpid(), getppid());
+	  }else{
+	     wait(NULL);
+	     wait(NULL);
+	     printf("Soy el padre\n ");
+	  }
+  }
+  exit(0);
+}
