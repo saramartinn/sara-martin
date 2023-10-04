ebcc80c1159f1e020f09656dcdc75a2c123f5bbf
saramartinn <145117681+saramartinn@users.noreply.github.com>
2023-10-04 09:50:50 +0200

    Add files via upload

---
+++ fork1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+void main()
+{
+   pid_t p2, p3, p4;
+   
+   if ((getpid() % 2) == 0)
+   {
+     	printf(" Soy el P1 y mi PID es = %d, y el de mi padre es = %d\n", getpid(), getppid());
+   }
+   else
+   {
+     	printf(" Soy el P1 y mi PID es = %d \n", getpid());
+   }
+   
+   p2 = fork();
+   
+   if (p2 == 0)
+   {
+     	if ((getpid() % 2) == 0)
+     	{
+     		printf(" Soy el P2 y mi PID es = %d, y el de mi padre es = %d\n", getpid(), getppid());
+     	}
+     	else
+     	{
+     		printf(" Soy el P2 y mi PID es = %d \n", getpid());
+     	}
+   }
+   else
+   {
+      p3 = fork();
+      if (p3 == 0)
+      {
+      	p4 = fork();
+      	if (p4 == 0){
+      		if ((getpid() % 2) == 0)
+      		{
+     			printf(" Soy el P4 y mi PID es = %d, y el de mi padre es = %d\n", getpid(), getppid());
+     		}	
+     		else
+     		{
+     			printf(" Soy el P4 y mi PID es = %d \n", getpid());
+     		}
+      	}
+      	else
+      	{
+      		wait(NULL);
+      		if ((getpid() % 2) == 0)
+      		{
+     			printf(" Soy el P3 y mi PID es = %d, y el de mi padre es = %d\n", getpid(), getppid());
+     		}
+     		else
+     		{
+     			printf(" Soy el P3 y mi PID es = %d \n", getpid());
+     		}
+      	}
+      }
+      else
+      {
+      	wait(NULL);
+      	wait(NULL);
+      	
+      }
+   }
+   exit(0);
+}
+
